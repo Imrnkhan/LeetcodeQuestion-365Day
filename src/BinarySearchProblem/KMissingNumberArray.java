205e08e8d629bf088c45a1a500c0996292037e4a
91905 <imrankhan7581@gmail.com>
2023-03-12 12:57:52 +0530

    Merge remote-tracking branch 'origin/master'

--- src/BinarySearchProblem/KMissingNumberArray.java
+++ src/BinarySearchProblem/KMissingNumberArray.java
@@ -7,14 +7,33 @@ public class KMissingNumberArray {
         System.out.println(test(arr,k));
     }
     public static int test(int[] arr,int k){
-        for(int i =0;i< arr.length;i++){
-            if(arr[i]<=k){
-                k++;
-            }
+//        for(int i =0;i< arr.length;i++){
+//            if(arr[i]<=k){
+//                k++;
+//            }
+//        }
+//        return k;
+
+        int start=0;
+
+        int end=arr.length-1;
+
+        while(start<=end){
+
+            int mid=start+(end-start)/2;
+
+            if(arr[mid]-mid-1<k)
+
+                start=mid+1;
+
+            else end=mid-1;
+
         }
-        return k;
+
+        return k+start;
     }
-}
+    }
+
 
 
 
