7f03265ec8c1b32cb34df74d42609d0acd535c06
91905 <imrankhan7581@gmail.com>
2023-03-20 23:20:56 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/LinklistQuestion/RemoveDuplicateSortedArrayLinklist.java
@@ -0,0 +1,43 @@
+package LinklistQuestion;
+
+public class RemoveDuplicateSortedArrayLinklist {
+//    public static void main(String[] args) {
+
+//    }
+//    public ListNode deleteDuplicate(ListNode head){
+//
+//        ListNode current = head;
+//
+//        while (current!=null && current.next!=null){
+//            if(current.next == current.next.val){
+//                current.next = current.next.next;
+//            }
+//            else {
+//                current = current.next;
+//            }
+//        }
+//        return head;
+//    }
+}
+
+/*
+83. Remove Duplicates from Sorted List
+Easy
+7K
+244
+Companies
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+ */
