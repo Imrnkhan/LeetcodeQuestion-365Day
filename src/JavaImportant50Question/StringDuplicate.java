041db270243c0f335eb88f974335d4e275e342f3
91905 <imrankhan7581@gmail.com>
2023-03-24 21:12:55 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/JavaImportant50Question/StringDuplicate.java
@@ -0,0 +1,27 @@
+package JavaImportant50Question;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringDuplicate {
+    public static void main(String[] args) {
+        String str = "Leetcode";
+
+        System.out.println(test(str));
+    }
+    public static String test(String str) {
+        Set<Character> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!set.contains(c)) {
+                set.add(c);
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
