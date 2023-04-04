ac2aea7f55db2ed3a1e17acb8b0246287b849411
91905 <imrankhan7581@gmail.com>
2023-04-04 22:46:50 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Leetcode/LongestContinuesINcresingSubSequence.java
@@ -0,0 +1,60 @@
+package Leetcode;
+
+public class LongestContinuesINcresingSubSequence {
+    public static void main(String[] args) {
+        int [] arr = {1,3,5,4,7};
+        System.out.println(test(arr));
+    }
+    public static int test(int[]arr){
+  int max = Integer.MIN_VALUE;
+  int count =0;
+  int maxcount =0;
+  for(int i =0;i< arr.length;i++){
+      if(arr[i]>max){
+          count++;
+      }
+      else {
+          count =1;
+      }
+      max = arr[i];
+      maxcount = Math.max(maxcount,count);
+
+  }
+        return maxcount;
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+674. Longest Continuous Increasing Subsequence
+Easy
+2.1K
+170
+Companies
+Given an unsorted array of integers nums, return the length of the longest continuous increasing subsequence (i.e. subarray). The subsequence must be strictly increasing.
+
+A continuous increasing subsequence is defined by two indices l and r (l < r) such that it is [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] and for each l <= i < r, nums[i] < nums[i + 1].
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,4,7]
+Output: 3
+Explanation: The longest continuous increasing subsequence is [1,3,5] with length 3.
+Even though [1,3,5,7] is an increasing subsequence, it is not continuous as elements 5 and 7 are separated by element
+4.
+Example 2:
+
+Input: nums = [2,2,2,2,2]
+Output: 1
+Explanation: The longest continuous increasing subsequence is [2] with length 1. Note that it must be strictly
+increasing.
+ */
