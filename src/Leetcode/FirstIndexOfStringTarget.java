410b70ba4ce9200e5505c63abfcdc37b00e8efd6
91905 <imrankhan7581@gmail.com>
2023-04-05 22:28:29 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Leetcode/FirstIndexOfStringTarget.java
@@ -0,0 +1,36 @@
+package Leetcode;
+
+public class FirstIndexOfStringTarget {
+    public static void main(String[] args) {
+        String str = "leetcode";
+        String target = "code";
+        System.out.println(test(str,target));
+    }
+    public static int test(String str, String target){
+        return str.indexOf(target);
+    }
+}
+
+
+/*
+28. Find the Index of the First Occurrence in a String
+Easy
+3.2K
+153
+Companies
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ */
