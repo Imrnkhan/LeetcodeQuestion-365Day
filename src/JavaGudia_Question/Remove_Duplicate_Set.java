9cbacf4d0e76f8a019a1be44f4ba531ca2c44715
91905 <imrankhan7581@gmail.com>
2023-04-07 05:13:38 +0530

    Merge remote-tracking branch 'origin/master'

--- src/JavaGudia_Question/Remove_Duplicate_Set.java
+++ src/JavaGudia_Question/Remove_Duplicate_Set.java
@@ -6,13 +6,14 @@ import java.util.Set;
 public class Remove_Duplicate_Set {
     public static void main(String[] args) {
         int[] arr = {52,14,65,1,2,3,5,6,2,3,6,4,8,};
-     duplicate(arr);
+        System.out.println(duplicate(arr));
     }
-    public static void duplicate(int[] arr){
+    public static Set duplicate(int[] arr){
         Set hs = new HashSet<>();
         for(int i =0;i<arr.length;i++){
             hs.add(arr[i]);
         }
-        hs.forEach(emel-> System.out.print(emel+" "));
+        return hs;
     }
+
 }
