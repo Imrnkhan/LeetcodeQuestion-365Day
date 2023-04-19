e2ba033c03eea294f4f20c2a3024dbcf1bb37883
91905 <imrankhan7581@gmail.com>
2023-04-20 04:58:17 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Leetcode/DayOfWeek.java
@@ -0,0 +1,15 @@
+package Leetcode;
+
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        int dayNum = 5;
+        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        System.out.println(days[dayNum]);
+    }
+}
+
+
+
+//3 = Wednesday
+//5 = Friday
