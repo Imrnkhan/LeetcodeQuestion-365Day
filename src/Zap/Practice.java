ed9ca94f8b80e6bbc78840815bd3736b26e510c5
91905 <imrankhan7581@gmail.com>
2023-05-19 16:25:32 +0530

    Merge remote-tracking branch 'origin/master'

--- src/Zap/Practice.java
+++ src/Zap/Practice.java
@@ -1,20 +1,25 @@
 package Zap;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
-
 
 public class Practice {
     public static void main(String[] args) {
-
-
+ int [] arr = {-1,-3,-4,2,4,5};
+        System.out.println(test(arr));
     }
+    public static int test(int[] arr){
+        int pos = 0;
+        int neg = 0;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]>0){
+                pos++;
+            } else if (arr[i]<0) {
+                neg--;
 
-    public static int test(String str) {
-        return 0;
+            }
+        }
+            return Math.max(pos,neg);
     }
-}
+    }
 
 
 
