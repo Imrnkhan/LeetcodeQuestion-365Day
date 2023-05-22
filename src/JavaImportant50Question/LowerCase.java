6e0a59d52498bd8c2d270182cd0669f16de8ab6a
91905 <imrankhan7581@gmail.com>
2023-05-22 16:11:39 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/JavaImportant50Question/LowerCase.java
@@ -0,0 +1,17 @@
+package JavaImportant50Question;
+
+public class LowerCase {
+    public static void main(String[] args) {
+        String str = "LEETCODE";
+        System.out.println(test(str));
+    }
+    public static String test(String str){
+//        return str.toLowerCase();
+        StringBuffer hs =  new StringBuffer();
+        for(int i =0;i<str.length();i++){
+            hs.append(Character.toLowerCase(str.charAt(i)));
+        }
+        return hs.toString();
+    }
+}
+
