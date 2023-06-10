d0dde1688a23add82657e2e90110513d7612294e
91905 <imrankhan7581@gmail.com>
2023-06-10 16:42:58 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/JavaImportant50Question/Clockwise90Rotate.java
@@ -0,0 +1,22 @@
+package JavaImportant50Question;
+
+public class Clockwise90Rotate {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        rotateBy90(arr);
+    }
+    public static void rotateBy90(int[][] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            StringBuilder str = new StringBuilder();
+            for (int j = arr.length - 1; j >= 0; j--) {
+                str.append(arr[j][i]).append(" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
