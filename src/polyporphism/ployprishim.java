0cb6ae15b40b7200f5acbc92fa767fb1e2a17f04
91905 <imrankhan7581@gmail.com>
2023-07-02 23:26:11 +0530

    Merge remote-tracking branch 'origin/master'

--- src/polyporphism/ployprishim.java
+++ src/polyporphism/ployprishim.java
@@ -19,3 +19,33 @@ public class ployprishim {
 
 //Defining more than one functionality with the same name in the same class is known as
 //polymorphism.
+
+//class Shape {
+//    void draw() {
+//        System.out.println("Drawing a shape");
+//    }
+//}
+
+//class Circle extends Shape {
+//    @Override
+//    void draw() {
+//        System.out.println("Drawing a circle");
+//    }
+//}
+//
+//class Rectangle extends Shape {
+//    @Override
+//    void draw() {
+//        System.out.println("Drawing a rectangle");
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Shape shape1 = new Circle();
+//        Shape shape2 = new Rectangle();
+//
+//        shape1.draw();  // Output: Drawing a circle
+//        shape2.draw();  // Output: Drawing a rectangle
+//    }
+//}
