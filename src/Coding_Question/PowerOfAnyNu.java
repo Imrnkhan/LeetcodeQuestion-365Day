eae44ce5ed72bfc5acd649164b58b3430326776d
91905 <imrankhan7581@gmail.com>
2023-07-22 11:48:20 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Coding_Question/PowerOfAnyNu.java
@@ -0,0 +1,11 @@
+package Coding_Question;
+
+public class PowerOfAnyNu {
+    public static void main(String[] args) {
+        int base = 12;
+        int exponent = 2;
+        int result = (int) Math.pow(base,exponent);
+
+        System.out.println(result);
+    }
+}
