8bfad1be847dbf7019c755ededd4c61fea5fdb2e
91905 <imrankhan7581@gmail.com>
2023-08-11 23:05:21 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/CodeForces/ReverseInteger.java
@@ -0,0 +1,18 @@
+package CodeForces;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        int num = 123;
+        System.out.println(test(num));
+    }
+    public static int test(int num){
+        int rev = 0;
+        while (num!=0){   // 123!=0 condition true
+            int digit = num%10;      //  123%10 = 3
+            rev = rev*10+digit;      //0*10+3 => rev = 3
+            num = num/10;             // left 123/10 => 12 again same proces
+
+        }
+        return rev;
+    }
+}
