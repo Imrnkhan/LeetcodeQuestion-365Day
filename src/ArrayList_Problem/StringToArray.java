3f2c8565c8faf31eddd2d79c8cd2252a68a58a39
91905 <imrankhan7581@gmail.com>
2023-08-16 15:06:23 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ArrayList_Problem/StringToArray.java
@@ -0,0 +1,15 @@
+package ArrayList_Problem;
+
+public class StringToArray {
+        public static void main(String[] args) {
+            String str = "Hello, World!";
+            char[] arr = str.toCharArray();
+
+            // Printing each character in the array
+            for (int i =0;i<arr.length;i++) {
+                System.out.print(arr[i]+ " ");
+            }
+        }
+    }
+
+
