9af5111d8e70556fcc445bea08ee0ab382535545
91905 <imrankhan7581@gmail.com>
2023-08-23 00:18:22 +0530

    Merge remote-tracking branch 'origin/master'

--- src/Zap/Prime.java
+++ src/Zap/Prime.java
@@ -1,19 +1,24 @@
 package Zap;
 
-public class Prime {
-    public static void main(String[] args) {
-        int num = 4;
-        System.out.println(test(num));
+abstract class Prime {
+     abstract void eat();
+
     }
-    public static boolean test(int num){
-        if(num==0 || num==1){
-            return false;
-                }
-            for(int i =2;i<num;i++){
-                if(num%i==0){
-                    return false;
-            }
+    class doh extends Prime{
+
+        @Override
+        void eat() {
+
+            System.out.println("heelo");
         }
-        return true;
+
+        public static void main(String[] args) {
+            Prime hs = new doh();
+            hs.eat();
+        }
     }
-}
+
+
+
+
+
