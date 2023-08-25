695cec95ebb8a0766ac5403556ced8167ff44195
91905 <imrankhan7581@gmail.com>
2023-08-25 18:32:31 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Coding_Question/abstractClass.java
@@ -0,0 +1,20 @@
+package Coding_Question;
+
+abstract class abstractClass {
+
+    abstract void test();
+}
+
+class next extends abstractClass{
+
+    @Override
+    void test() {
+        System.out.println("this method abstract");
+
+    }
+
+    public static void main(String[] args) {
+        abstractClass abs = new next();
+        abs.test();
+    }
+}
