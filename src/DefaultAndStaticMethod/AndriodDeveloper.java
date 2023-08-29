30baf52779b68df34173cb60872313df6c4a4383
91905 <imrankhan7581@gmail.com>
2023-08-30 02:18:09 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/DefaultAndStaticMethod/AndriodDeveloper.java
@@ -0,0 +1,14 @@
+package DefaultAndStaticMethod;
+
+public class AndriodDeveloper implements Developer{
+    @Override
+    public void develop() {
+        System.out.println("i am andriod developer");
+    }
+
+    @Override
+    public void test() {
+        System.out.println("i am andriod test developer");
+
+    }
+}
