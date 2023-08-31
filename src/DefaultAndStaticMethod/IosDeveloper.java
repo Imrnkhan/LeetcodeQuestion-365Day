15337d9575c707a943db794e3b4ac3a7ff98a535
91905 <imrankhan7581@gmail.com>
2023-08-31 23:02:26 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/DefaultAndStaticMethod/IosDeveloper.java
@@ -0,0 +1,14 @@
+package DefaultAndStaticMethod;
+
+public class IosDeveloper implements Developer{
+    @Override
+    public void develop() {
+        System.out.println("i am ios developer");
+    }
+
+    @Override
+    public void test() {
+        System.out.println("i am ios testing");
+
+    }
+}
