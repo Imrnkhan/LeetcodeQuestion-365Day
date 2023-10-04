c519db0dff907a79976ee9a87ee74e4c02f5fd79
91905 <imrankhan7581@gmail.com>
2023-10-04 23:18:04 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/JavaImportant50Question/DuplicatePrint.java
@@ -0,0 +1,24 @@
+package JavaImportant50Question;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class DuplicatePrint {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 2, 3, 5, 2, 6, 8, 9, 4, 9};
+      test(arr);
+    }
+     public static void test(int arr[]) {
+         HashSet<Integer> seen = new HashSet<>();
+         HashSet<Integer> duplicates = new HashSet<>();
+
+         for (int num : arr) {
+             if (!seen.add(num)) {
+                 // num is a duplicate, but check if it is already in the duplicates set
+                 if (duplicates.add(num)) {
+                     System.out.print(num + " ");
+                 }
+             }
+         }
+    }
+}
