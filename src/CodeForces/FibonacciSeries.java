5f09a6e59d846e8d410485fd546a2c466e239c70
91905 <imrankhan7581@gmail.com>
2023-10-13 23:21:50 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/CodeForces/FibonacciSeries.java
@@ -0,0 +1,48 @@
+package CodeForces;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 15; // Number of Fibonacci numbers to generate
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        System.out.print(firstNumber + " " + secondNumber + " "); // Print the first two numbers
+
+        for (int i = 3; i <= n; i++) {
+            int nextNumber = firstNumber + secondNumber;
+            System.out.print(nextNumber + " ");
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+        }
+    }
+}
+
+
+
+
+//another way 
+
+/*
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 15; // Number of Fibonacci numbers to generate
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+
+
+ */
