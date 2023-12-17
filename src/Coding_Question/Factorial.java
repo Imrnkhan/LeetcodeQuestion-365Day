47090ded4622648581279e72e617fb1e51b8b59e
91905 <imrankhan7581@gmail.com>
2023-12-17 17:22:55 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Coding_Question/Factorial.java
@@ -0,0 +1,15 @@
+package Coding_Question;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        System.out.println(test(arr));
+    }
+    public static int test(int[] arr){
+        int sum = 1;
+        for(int i = 0;i<arr.length;i++){
+            sum= sum*arr[i];
+        }
+        return sum;
+    }
+}
